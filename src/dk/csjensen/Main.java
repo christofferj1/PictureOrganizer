8f5403a92faeb56c2803bce6871eea158790f48a
Christoffer Jensen <christofferj1@gmail.com>
2024-02-06 09:23:44 +0100

    Handle file name/date time collisions

--- src/dk/csjensen/Main.java
+++ src/dk/csjensen/Main.java
@@ -6,7 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -50,15 +52,16 @@ public class Main
 
   private static void moveFiles(final File directory, final boolean rename) throws IOException
   {
+    final List<String> fileNamesUsed = new ArrayList<>();
     try (Stream<Path> paths = Files.list(directory.toPath())) {
       for (Path path : paths.toList()) {
         BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
         final FileTime fileTime = attributes.lastModifiedTime();
         final String month = fileTime.toString().substring(5, 7); // String format is yyyy-MM-dd...
 
-        final String targetFileName = rename && fileNameIsNotDateTime(path.getFileName().toString())
-            ? dateFileName(path, fileTime.toString())
-            : path.getFileName().toString();
+        final String targetFileName = rename && fileNameIsNotDateTime(path.getFileName().toString()) ? dateFileName(path, fileTime.toString(), fileNamesUsed) : path.getFileName().toString();
+
+        fileNamesUsed.add(targetFileName);
 
         final Path target = Path.of(month, targetFileName);
 
@@ -74,12 +77,31 @@ public class Main
     return !Pattern.matches("\\d{8}_\\d{6}\\..+", fileName);
   }
 
-  private static String dateFileName(final Path path, final String fileTime)
+  private static String dateFileName(final Path path, final String fileTime, final List<String> fileNamesUsed)
   {
     final String date = fileTime.substring(0, 10).replace("-", "");
     final String time = fileTime.substring(11, 19).replace(":", "");
 
     final String file_extension = Arrays.stream(path.getFileName().toString().split("\\.")).toList().getLast();
-    return String.format("%s_%s.%s", date, time, file_extension);
+    String fileName = String.format("%s_%s.%s", date, time, file_extension);
+
+    fileName = handleFileNameCollisions(fileNamesUsed, fileName);
+
+    return fileName;
+  }
+
+  private static String handleFileNameCollisions(List<String> fileNamesUsed, String fileName)
+  {
+    // Check if the file name exists on the target, and if so, add a number like this:
+    // 20240206_123456.jpg -> 20240206_123456_1.jpg (increment the number when multiple duplicates exists)
+    while (fileNamesUsed.contains(fileName)) {
+      final String[] fileNameParts = fileName.split("[_\\.]"); // Split on both _ and .
+      if (fileNameParts.length == 3) // Add number one to duplicate
+        fileName = String.format("%s_%s_1.%s", fileNameParts[0], fileNameParts[1], fileNameParts[2]);
+      else if (fileNameParts.length == 4) // Increment the number to handle multiple duplicates
+        fileName = String.format("%s_%s_%d.%s", fileNameParts[0], fileNameParts[1], Integer.parseInt(fileNameParts[2]) + 1, fileNameParts[3]);
+      else throw new RuntimeException("Date-time file name has weird format: " + fileName);
+    }
+    return fileName;
   }
 }
